a93abf4e8aba2dd5f093efc7eb960f07fbd71a2f
Ryan Santiago <cr7rs7@gmail.com>
2023-05-05 20:06:25 -0400

    Add proper routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -5,12 +8,16 @@ import Articles from './pages/Articles'
 
 function App() {
   return (
-    <>
-      <Home />
-      <About />
-      <Contact />
-      <Articles />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/articles' element={<Articles />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
