53da1d706ac0959f41ed53666fb7e6e2de17ab3d
CamSanti16 <75475520+CamSanti16@users.noreply.github.com>
2023-05-16 20:07:57 -0400

    Input form worked

--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './signin.scss'
 
 const SignIn = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const firebaseSignIn = (e) => {
+        console.log("this is the email " + email)
+    }
   return <div className='background'>
     <div className='signInSection'> 
         <div className='signInBox'>
                 SignIn
                 <div className="formWrapper">
                     <span className="inputWrapper">
-                        <input className="centerWrapper inputBox" placeholder='Username or E-mail'>
+                        <input value={email} onChange={(event) => setEmail(event.target.value)} type="text" className="centerWrapper inputBox" placeholder='Username or E-mail'>
 
                         </input>
                     </span>
                     <span className="inputWrapper">
-                        <input className=" centerWrapper inputBox" placeholder='Password'>
+                        <input value={password} onChange={(event) => setPassword(event.target.value)} type="text" className="centerWrapper inputBox" placeholder='Password'>
 
                         </input>
                     </span>
                     <span className="inputWrapper">
-                        <button className="centerWrapper signInButton">
+                        <button onClick={firebaseSignIn} className="centerWrapper signInButton">
                             Sign In
                         </button>
                     </span>
@@ -29,6 +36,14 @@ const SignIn = () => {
                         <span>
                             Sign Up 
                         </span>
+                    </span>
+                    <span>
+                        or you can sign in with 
+                    </span>
+                    <span>
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-google"></i>
+
                     </span>
                 </div>
         </div>
